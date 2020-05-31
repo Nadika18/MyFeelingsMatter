2f61e2e77e220824bad6dc5d2014451344b09f6b
Krishbin Paudel <krishbinp@outlook.com>
2020-05-31 15:21:22 +0545

    final

--- mentor/urls.py
+++ mentor/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
 	path('',views.home,name='mentor-home'),
-<<<<<<< HEAD
 	path('create',views.patient_create, name="mentor-form"),
-=======
-	path('form.html',views.form, name="mentor-form"),
 	path('depression.html',views.dep, name="mentor-dep")
->>>>>>> 22f1f180c2f26b59ac5008d5a948eb268b8d0efb
+
 ]
