4be7a20cbd06af3395b4b54d5769d6025353c159
Nadika Poudel <nadikapoudel16@gmail.com>
2020-05-31 07:17:40 +0545

    form loaded as link

--- mentor/urls.py
+++ mentor/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
 	path('',views.home,name='mentor-home'),
+	path('form.html',views.form, name="mentor-form"),
 ]
--- mentor/views.py
+++ mentor/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
 	return render(request,'mentor/home.html')
+def form(request):
+	return render(request,'mentor/form.html')
 
 # Create your views here.
